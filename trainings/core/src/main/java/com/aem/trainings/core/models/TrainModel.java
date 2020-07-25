cb8954eee0d8c3b4668b5d2c8a298d17851d0d23
Hima Naga Uday Kiran <amirapuuday169@gmail.com>
2020-07-25 13:11:45 +0530

    Update TrainModel.java

--- trainings/core/src/main/java/com/aem/trainings/core/models/TrainModel.java
+++ trainings/core/src/main/java/com/aem/trainings/core/models/TrainModel.java
@@ -1,12 +1,13 @@
 package com.aem.trainings.core.models;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Model(adaptables = { Resource.class,
 	    SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
@@ -19,10 +20,10 @@ public class TrainModel {
 	@Inject
 	@Via("resource")
 	private String count;
-	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	@PostConstruct
 	  private void init() {
-		
+		log.info("inside active metod");
 	}
 
 	public String getText() {
